14b1f9d080d109e6a8ef422524d5cb42eadf73d5
Flaque <evanjamesconrad@gmail.com>
2020-03-01 12:05:05 -0800

    Fix lint

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,55 +1,52 @@
-import { from, save } from "automerge";
-import nock from "nock";
-import RoomServiceClient from "./client";
-import Offline from "./offline";
-import Sockets from "./socket";
-import { injectFakeSocket } from "./test-socket";
-import { uniq } from "lodash";
+import { from, save } from 'automerge';
+import nock from 'nock';
+import RoomServiceClient from './client';
+import Offline from './offline';
+import Sockets from './socket';
+import { injectFakeSocket } from './test-socket';
+import { uniq } from 'lodash';
 
-const URL = "https://coolsite.com";
-jest.mock("idb-keyval");
+const URL = 'https://coolsite.com';
+jest.mock('idb-keyval');
 
 function mockAuthEndpoint(stateStr?: string) {
   return nock(URL)
-    .post("/api/roomservice")
+    .post('/api/roomservice')
     .reply(200, {
       room: {
-        id: "id",
-        reference: "my-room",
-        state: stateStr || "{}"
+        id: 'id',
+        reference: 'my-room',
+        state: stateStr || '{}',
       },
       session: {
-        token: "short-lived-token"
-      }
+        token: 'short-lived-token',
+      },
     });
 }
 
-it("should call the authorization endpoint when creating a room", async () => {
-  const socket = injectFakeSocket();
+it('should call the authorization endpoint when creating a room', async () => {
   const scope = mockAuthEndpoint();
   const client = new RoomServiceClient({
-    authUrl: URL + "/api/roomservice"
+    authUrl: URL + '/api/roomservice',
   });
-  const room = client.room("my-room");
+  const room = client.room('my-room');
 
   await room.init();
 
   expect(scope.isDone()).toBeTruthy();
 });
 
-test("room gets called with bearer token", async () => {
+test('room gets called with bearer token', async () => {
   mockAuthEndpoint();
-  const mock = jest
-    .spyOn(Sockets, "newSocket")
-    .mockImplementation((url, connectopts) => {
-      // @ts-ignore
-      return { on: jest.fn() } as SocketIOClient.Socket;
-    }).mock;
+  const mock = jest.spyOn(Sockets, 'newSocket').mockImplementation(() => {
+    // @ts-ignore
+    return { on: jest.fn() } as SocketIOClient.Socket;
+  }).mock;
 
   const client = new RoomServiceClient({
-    authUrl: URL + "/api/roomservice"
+    authUrl: URL + '/api/roomservice',
   });
-  const room = client.room("my-room");
+  const room = client.room('my-room');
   await room.init();
 
   const urls = mock.calls.map(([url]) => url);
@@ -57,83 +54,83 @@ test("room gets called with bearer token", async () => {
 
   expect(uniq(urls.sort())).toStrictEqual(
     [
-      "https://api.roomservice.dev/v1/doc",
-      "https://api.roomservice.dev/v1/presence"
+      'https://api.roomservice.dev/v1/doc',
+      'https://api.roomservice.dev/v1/presence',
     ].sort()
   );
 
   // @ts-ignore because bad typings make me sad
   expect(args[0].transportOptions.polling.extraHeaders.authorization).toBe(
-    "Bearer short-lived-token"
+    'Bearer short-lived-token'
   );
 });
 
-test("room.publish() can change a document", async () => {
+test('room.publish() can change a document', async () => {
   mockAuthEndpoint();
 
   const client = new RoomServiceClient({
-    authUrl: URL + "/api/roomservice"
+    authUrl: URL + '/api/roomservice',
   });
 
-  const room = client.room("my-room");
+  const room = client.room('my-room');
   const sockets = injectFakeSocket();
   await room.init();
-  sockets.emit("connect");
+  sockets.emit('connect');
 
   const newState = await room.setDoc((prevState: any) => {
-    prevState.someOption = "hello!";
+    prevState.someOption = 'hello!';
   });
 
-  expect(newState.someOption).toBe("hello!");
+  expect(newState.someOption).toBe('hello!');
 });
 
-test("room.restore() attempts to restore from offline", async () => {
+test('room.restore() attempts to restore from offline', async () => {
   const client = new RoomServiceClient({
-    authUrl: URL + "/api/roomservice"
+    authUrl: URL + '/api/roomservice',
   });
-  const room = client.room("my-room");
+  const room = client.room('my-room');
 
   // @ts-ignore because trust me typescript, I am very wise and have
   // been on this earth longer than thee, and I, the great programmer,
   // know for certain that window.indexDB is, in fact, equal to
   // wiggly-woggle-pop.
-  window.indexedDB = "wiggly-woggle-pop";
+  window.indexedDB = 'wiggly-woggle-pop';
 
-  jest.spyOn(Offline, "getDoc").mockImplementation(async (ref, doc) => {
-    return save(from({ name: "offlinedoc" }));
+  jest.spyOn(Offline, 'getDoc').mockImplementation(async () => {
+    return save(from({ name: 'offlinedoc' }));
   });
 
   const doc = await room.restore();
-  expect(doc).toEqual({ name: "offlinedoc" });
+  expect(doc).toEqual({ name: 'offlinedoc' });
 });
 
-test("room.init() will merge online data with offline data", async () => {
+test('room.init() will merge online data with offline data', async () => {
   const client = new RoomServiceClient({
-    authUrl: URL + "/api/roomservice"
+    authUrl: URL + '/api/roomservice',
   });
-  const room = client.room("my-room");
+  const room = client.room('my-room');
 
   // setup offline
-  jest.spyOn(Offline, "getDoc").mockImplementation(async (ref, doc) => {
-    return save(from({ offline: "offline" }));
+  jest.spyOn(Offline, 'getDoc').mockImplementation(async () => {
+    return save(from({ offline: 'offline' }));
   });
 
   // setup online
-  mockAuthEndpoint(save(from({ online: "online" })));
+  mockAuthEndpoint(save(from({ online: 'online' })));
 
   const { doc } = await room.init();
   expect(doc).toEqual({
-    offline: "offline",
-    online: "online"
+    offline: 'offline',
+    online: 'online',
   });
 });
 
-test("room.onUpdateDoc callback tries to save the document to offline", async done => {
+test('room.onUpdateDoc callback tries to save the document to offline', async done => {
   mockAuthEndpoint();
   const client = new RoomServiceClient({
-    authUrl: URL + "/api/roomservice"
+    authUrl: URL + '/api/roomservice',
   });
-  const room = client.room("my-room");
+  const room = client.room('my-room');
 
   const cb = jest.fn();
   room.onSetDoc(cb);
@@ -145,21 +142,21 @@ test("room.onUpdateDoc callback tries to save the document to offline", async do
   await room.init();
 
   // @ts-ignore private; we'd normally get this from the auth endpoint
-  room._roomId = "my-room-id";
+  room._roomId = 'my-room-id';
 
-  const setOffline = jest.spyOn(Offline, "setDoc");
+  const setOffline = jest.spyOn(Offline, 'setDoc');
 
   onUpdateSocket!(
     JSON.stringify({
       meta: {
-        roomId: "my-room-id"
+        roomId: 'my-room-id',
       },
       payload: {
         msg: {
           clock: new Map(),
-          docId: "default"
-        }
-      }
+          docId: 'default',
+        },
+      },
     })
   );
 

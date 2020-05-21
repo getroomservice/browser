29b2ee29790f75e03fcc359de58ff697fe46b181
Evan Conrad <evanjamesconrad@gmail.com>
2020-05-21 09:22:03 -0700

    No reupdates (#6)

--- src/socket.ts
+++ src/socket.ts
@@ -25,6 +25,10 @@ const Sockets = {
     socket.on(event, fn);
   },
 
+  off(socket: SocketIOClient.Socket, event: string) {
+    socket.off(event);
+  },
+
   emit(
     socket: SocketIOClient.Socket,
     event: 'sync_room_state' | 'update_presence' | 'authenticate',

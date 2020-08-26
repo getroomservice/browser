427975fd37a9bf900026cf19af440af63bf79339
Flaque <evanjamesconrad@gmail.com>
2020-08-26 16:20:57 -0700

    use main

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const WS_URL = 'ws://localhost:3452';
-export const DOCS_URL = 'http://localhost:3454';
+// export const WS_URL = 'ws://localhost:3452';
+// export const DOCS_URL = 'http://localhost:3454';
 
-// export const WS_URL = 'wss://super.roomservice.dev/ws';
-// export const DOCS_URL = 'https://super.roomservice.dev';
+export const WS_URL = 'wss://super.roomservice.dev/ws';
+export const DOCS_URL = 'https://super.roomservice.dev';

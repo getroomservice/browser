3826a9665b5e61bbc634d8f95693e826c7d07e09
Flaque <evanjamesconrad@gmail.com>
2020-09-15 13:50:34 -0400

    fix escape

--- src/escape.ts
+++ src/escape.ts
@@ -3,13 +3,5 @@ export function escape(value: number | string | object): string {
 }
 
 export function unescape(value: string): string | number | object {
-  if (
-    value.length >= 2 &&
-    value[0] === '"' &&
-    value[value.length - 1] === '"'
-  ) {
-    return value.slice(1, value.length - 1);
-  }
-
   return JSON.parse(value);
 }
